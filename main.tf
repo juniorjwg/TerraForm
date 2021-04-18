c21ecfae87358363f4e2e91c0ecc5e60033a860a
juniorjwg <junior.jwg@gmail.com>
2021-04-17 22:13:02 -0300

    a

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "var.my-access-key"
-  secret_key = "var.my-secret-key"
+  access_key = "AKIAYJN5Y3SLEBXMNE5A"
+  secret_key = "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"
   region  = "us-east-2"
 }
 
