069c2657ca349885b89a58641c54e2306dc8ca71
juniorjwg <junior.jwg@gmail.com>
2021-04-17 21:11:17 -0300

    variable

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "var.my-access-key"
-  secret_key = "var.my-secret-key"
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
   region  = "us-east-2"
 }
 
